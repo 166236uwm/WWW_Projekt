be64b1e2598fe2754b28beaf77bf84700f7a5b3c
Icol <94836511+Icolinio@users.noreply.github.com>
2024-11-21 09:21:57 +0100

    Dodanie modeli

--- Projekt/Projekt/models.py
+++ Projekt/Projekt/models.py
@@ -12,3 +12,16 @@ class User(models.Model):
     surname = models.CharField(max_length=100, blank=False)
     role = models.CharField(max_length=100, blank=False)
     created_at = models.DateTimeField(default=timezone.now)
+
+class Transaction(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    POS = models.IntegerField(blank=False)
+    total_prize = models.FloatField(blank=False)
+    contents = models.CharField(max_length=500, blank=False)
+
+class Recipe(models.Model):
+    productID = models.IntegerField(blank=False)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    pricePerUnit = models.FloatField(blank=False)
